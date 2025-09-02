5a5247545aedfef6b856b63b21ff568f5cd93625
Kuldeep-Padshala <kuldeeplearning01@gmail.com>
2025-09-02 21:35:50 +0530

    Introduction to Graphs

---
+++ Concept029_Graphs/00-Intro.cpp
@@ -0,0 +1,24 @@
+// Graphs
+
+// so graphs are a collection of nodes and edges
+// nodes are also called vertices
+// edges are connections between nodes
+
+// a graph can be directed or undirected
+// a directed graph has edges that have a direction (from one node to another)
+// an undirected graph has edges that do not have a direction (the connection is bidirectional)
+
+// a graph can also be weighted or unweighted
+// a weighted graph has edges that have weights (costs, distances, etc.)
+// an unweighted graph has edges that do not have weights
+
+// a graph can be cyclic or acyclic
+// a cyclic graph has at least one cycle (a path that starts and ends at the same node)
+// an acyclic graph does not have any cycles
+
+// a graph can also be connected or disconnected
+// a connected graph has a path between every pair of nodes
+// a disconnected graph does not have a path between every pair of nodes
+
+// a graph can be represented in various ways
+// the most common representations are adjacency matrix and adjacency list
