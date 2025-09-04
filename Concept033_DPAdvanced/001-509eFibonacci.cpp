29c3363a020a35e81c501490f5afb54a1337015c
Kuldeep-Padshala <kuldeeplearning01@gmail.com>
2025-09-04 21:55:32 +0530

    Introduction to DP Problems

--- Concept033_DPAdvanced/001-509eFibonacci.cpp
+++ Concept033_DPAdvanced/001-509eFibonacci.cpp
@@ -17,20 +17,20 @@ class Solution {              // nth term of fibonacci
 public:
     int fib(int n) {
 
-        // DP TopDown: Recursion + Memorization
+        // ------------------ TopDown: Recursion + Memoization ------------------
         vector<int> dp(n+1, -1);
         return solve(n, dp);
 
-        // DP BottomUp : Tabulation
+        // ------------------  BottomUp: Tabulation  ---------------------
         vector<int> dp(n+1, -1);
         dp[0] = 0;
         dp[1] = 1;
         for(int i = 2; i <= n; ++i) {
-        dp[i] = dp[i-1] + dp[i-2];
+          dp[i] = dp[i-1] + dp[i-2];
         }
         return dp[n];
 
-        // Space Optimization
+        // ------------------ BottomUp Space Optimization  ---------------------
         vector<int> dp(3, 0);
         dp[0] = 0;
         dp[1] = 1;
